2b767c5bba892177172dec43d4d1754914744e64
danielsdev81 <danielsilverio81@utlook.com>
2024-04-19 18:38:12 -0300

    add: first model category

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import { sequelize } from "./database";
+import { adminjs, adminjsRouter } from "./adminjs";
 
 const app = express();
+
+app.use(express.static('public'))
+app.use(adminjs.options.rootPath, adminjsRouter);
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   sequelize.authenticate().then(() => {
-    console.log('DB connection successful');
-    
-  })
-  console.log(`Server started successfully at port ${PORT} \n http://localhost:3000`);
+    console.log("DB connection successful");
+  });
+  console.log(
+    `Server started successfully at port ${PORT} \n http://localhost:3000`
+  );
 });
