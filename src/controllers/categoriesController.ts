d26e38cfd216aa6161afd5cc3ba85c7bc5acd54b
Daniel Silverio <danielsilverio81@outlook.com>
2024-05-07 14:57:37 -0300

    add: get courses for category

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -3,11 +3,28 @@ import { categoryService } from "../services/categoryService";
 import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const categoriesController = {
+  //get /categories
   index: async (req: Request, res: Response) => {
-    const [ page, perPage ] = getPaginationParams(req.query);
+    const [page, perPage] = getPaginationParams(req.query);
     try {
-        const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
-        return res.json(paginatedCategories)
+      const paginatedCategories = await categoryService.findAllPaginated(
+        page,
+        perPage
+      );
+      return res.json(paginatedCategories);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+  //get /categories/:id
+  Show: async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+      const category = await categoryService.findByIdWithCourses(id);
+      return res.json(category);
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message });
