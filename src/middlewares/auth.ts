06f3dc06b810af33474381385a5534489adbf61c
Daniel Silverio <danielsilverio81@outlook.com>
2024-05-20 12:12:24 -0300

    changes

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,24 +5,59 @@ import { JwtPayload } from "jsonwebtoken";
 import { UserInstance } from "../models/User";
 
 export interface AuthenticationRequest extends Request {
-    user?: UserInstance | null
+    user?: UserInstance | null;
 }
 
-export function ensureAuth(req: AuthenticationRequest, res: Response, next: NextFunction) {
+export function ensureAuth(
+    req: AuthenticationRequest,
+    res: Response,
+    next: NextFunction
+) {
     const authorizationHeader = req.headers.authorization;
 
-    if (!authorizationHeader) return res.status(401).json({message: 'Não autorizado nenhum token encontrado!'});
+    if (!authorizationHeader)
+        return res
+            .status(401)
+            .json({ message: "Não autorizado nenhum token encontrado!" });
 
-    const token = authorizationHeader.replace(/Bearer /, '');
+    const token = authorizationHeader.replace(/Bearer /, "");
 
-    jwtService.verifyToken(token, (err, decoded) => {
-        if (err || typeof decoded === 'undefined') return res.status(401).json({
-            message: 'Não autorizado: token invalido'
-        })
+    jwtService.verifyToken(token, async (err, decoded) => {
+        if (err || typeof decoded === "undefined")
+            return res.status(401).json({
+                message: "Não autorizado: token invalido",
+            });
 
-        userService.findByEmail((decoded as JwtPayload).email).then(user => {
-            req.user = user;
-            next()
-        })
-    })
+        const user = await userService.findByEmail((decoded as JwtPayload).email);
+        req.user = user;
+        next();
+    });
+}
+
+export function ensureAuthByQuery(
+    req: AuthenticationRequest,
+    res: Response,
+    next: NextFunction
+) {
+    const { token } = req.query;
+    if (!token)
+        return res
+            .status(401)
+            .json({ message: "Não autorizado nenhum token encontrado!" });
+
+    if (typeof token !== "string")
+        return res
+            .status(400)
+            .json({ message: "O parâmetro token deve ser uma string" });
+
+    jwtService.verifyToken(token, async (err, decoded) => {
+        if (err || typeof decoded === "undefined")
+            return res.status(401).json({
+                message: "Não autorizado: token invalido",
+            });
+
+        const user = await userService.findByEmail((decoded as JwtPayload).email);
+        req.user = user;
+        next();
+    });
 }
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { categoriesController } from "./controllers/categoriesController";
 import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 import { authController } from "./controllers/authController";
-import { ensureAuth } from "./middlewares/auth";
+import { ensureAuth, ensureAuthByQuery } from "./middlewares/auth";
 
 const router = express.Router();
 
@@ -22,6 +22,6 @@ router.get("/courses/search", ensureAuth, coursesController.search);
 router.get("/courses/:id", ensureAuth, coursesController.show);
 
 //Episodes end Points
-router.get("/episodes/stream", episodesController.stream);
+router.get("/episodes/stream", ensureAuthByQuery, episodesController.stream);
 
 export { router };
